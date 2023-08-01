c613f39db3b81d0ab14028bc6842e250f90a9ec0
Natali Oleinik <natali.oleinik06@gmail.com>
2023-08-01 11:06:30 -0400

    Initial Commit

---
+++ testingGit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testingGit
+//
+//  Created by natali oleinik on 8/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("hi, Ranya!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testingGit/testingGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingGitApp.swift
+//  testingGit
+//
+//  Created by natali oleinik on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct testingGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
